0503f894237139c9b1d994ff57c0c4f63ffe9026
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2021-03-22 15:15:16 +0300

    Первое задание.

--- SentencesParserTask.cs
+++ SentencesParserTask.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -6,8 +13,34 @@ namespace TextAnalysis
     {
         public static List<List<string>> ParseSentences(string text)
         {
+            text = text.ToLower();
             var sentencesList = new List<List<string>>();
-            //...
+
+
+            char[] tochka = { '.', '?', '!', ';', ':', '(', ')'};
+            string[] sentences = text.Split(tochka, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var sent in sentences)
+            {
+                var list = new List<string>();
+                char[] points = new char[] { ' ', '^', '#', '$', '-', '+', '=', '\t', '\n', '\r',  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+                string[] words = sent.Split(points, StringSplitOptions.RemoveEmptyEntries);
+                foreach(var word in words)
+                {
+                    var sb = new StringBuilder();
+                    foreach(var symb in word)
+                    {
+                        if (Char.IsLetter(symb) || symb == '\'')
+                        {
+                            sb.Append(symb);
+                        }
+                    }
+                    string new_word = sb.ToString();
+                    list.Add(new_word);
+                }
+                sentencesList.Add(list);
+            }
+
+
             return sentencesList;
         }
     }
